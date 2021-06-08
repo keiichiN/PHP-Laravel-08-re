151fd8520330ea1f6ab6a867343bf6943c45aeca
EC2 Default User <ec2-user@ip-172-31-39-8.us-east-2.compute.internal>
2021-06-08 05:09:48 +0000

    PHP/Laravel 12 ユーザー認証を実装する.git

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,17 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 ///課題３
+
 
 ///課題４
 Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
