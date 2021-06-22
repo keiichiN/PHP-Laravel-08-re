bbdf663b9c1ad88282181bebe57c32ee97ca41c6
EC2 Default User <ec2-user@ip-172-31-39-8.us-east-2.compute.internal>
2021-06-22 04:25:25 +0000

    Laravel 14 投稿データを保存しよう再.git

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //
@@ -13,10 +15,44 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+                      $this->validate($request, Profile::$rules);
+
+      $Profile = new Profile;
+      $form = $request->all();
+
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $Profile->image_path = basename($path);
+      } else {
+          $Profile->image_path = null;
+      }
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+
+      // データベースに保存する
+      $Profile->fill($form);
+      $Profile->save();
         return redirect('admin/profile/create');
     }
+
+    public function index(Request $request)
+    {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('title', $cond_title)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
 
     public function edit()
     {
