a9a67df4920c2ad15269317a64bf940d9ae81d66
EC2 Default User <ec2-user@ip-172-31-39-8.us-east-2.compute.internal>
2021-06-30 12:27:04 +0000

    PHP/Laravel 19 一般ユーザーが読むニュースサイトを作成しよう

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,6 @@ Route::group(['prefix' => 'admin'], function() {
     Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
+
+Route::get('/', 'NewsController@index');
+Route::get('/', 'ProfileController@index');
